bbf49035278b948b90cfb864f9292b5e9c98d562
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-11-27 14:46:40 +0200

    Add files via upload

---
+++ Task12.5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task124
+{
+    class Program
+    {
+        static void FillQueue(Queue<int> q)
+        {
+            for(int i=1;i<=10;i++)
+            {
+                q.Enqueue(i);
+            }
+        }
+        static void PrintQueue(Queue<int> q)
+        {
+            while(q.Count!=0)
+            {
+                if(q.Peek()==5)
+                {
+                    Console.WriteLine("Number 5 is reached");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("{0}", q.Peek());
+                    q.Dequeue();
+                }
+            }
+        }
+        static void Main()
+        {
+            Queue<int> q = new Queue<int>();
+            FillQueue(q);
+            PrintQueue(q);
+        }
+    }
+}
